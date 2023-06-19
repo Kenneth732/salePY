d93dd7ef449b6d518a6c247f545d2d4486991d10
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-19 17:15:54 +0300

    import sales

--- sale/__init__.py
+++ sale/__init__.py
@@ -1,7 +1,4 @@
-# import the order module automatically
-import sales
-from sales.order import create_sales_order
-
-# default sales tax rate
-TAX_RATE = 0.07
-sales.order.create_sales_order()
+__all__ = [
+    "order",
+    'delivery'
+]
