3394c339614e3709ba5ab1d536d9dd89dec7566d
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-19 17:13:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from sales import TAX_RATE
+from sales.order import create_sales_order as create_order
+from sales.delivery import create_delivery as start_delivery
+from sales.billing import create_billing as issue_billing
+
+
+create_order()
+start_delivery()
+issue_billing()
+
+print(TAX_RATE)
